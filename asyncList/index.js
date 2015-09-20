5e7d6f49c585f9d1902cfc77733c6be2de97fc8d
Dianna Faulk <diannafaulk@gmail.com>
2015-09-19 17:08:06 -0700

    Pseudocode for async call

--- asyncList/index.js
+++ asyncList/index.js
@@ -3,6 +3,16 @@ var async = require('async');
 
 var app = express();
 
+app.get('/', function(req, res){
+  var urls = ['dianna.github.io','www.amazon.com','www.github.com','www.linkedin.com','www.hired.com','www.reddit.com','www.stackoverflow.com','www.angel.co/jobs','www.google.com'];
+  var results = [];
+  var asyncTasks = [];
+
+  // Add request functions for each url to asyncTask list
+
+  // After all asyncTasks are complete, return result order
+})
+
 var server = app.listen(3030, function(){
   var host = server.address().address;
   var port = server.address().port;
