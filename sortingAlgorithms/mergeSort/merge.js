53d164bc5f29552438c5d04d92dbfd944802c0f4
Dianna Faulk <diannafaulk@gmail.com>
2015-09-21 10:34:26 -0700

    Add example inputs/outputs

---
+++ sortingAlgorithms/mergeSort/merge.js
@@ -0,0 +1,16 @@
+
+/* 
+Arrays
+  mergeSort([5,4,2,6,8,0], function(a, b){ return a > b; });
+  // [8,6,5,4,2,0]
+
+Objects
+  var arr = [
+    {thing: 'a', order: 3},
+    {thing: 'a', order: 1},
+    {thing: 'a', order: 2}
+  ]
+  mergeSort(arr, function(a, b){ return a[order] < b[order] });
+*/
+
+var mergeSort = function(collection, rule){}
