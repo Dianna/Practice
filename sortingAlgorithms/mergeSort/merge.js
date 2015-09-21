3320af5d147df3b1f97b960330e865686fc2c0b9
Dianna Faulk <diannafaulk@gmail.com>
2015-09-21 10:51:40 -0700

    Edit array of object sample input

--- sortingAlgorithms/mergeSort/merge.js
+++ sortingAlgorithms/mergeSort/merge.js
@@ -6,11 +6,11 @@ Arrays
 
 Objects
   var arr = [
-    {thing: 'a', order: 3},
-    {thing: 'a', order: 1},
-    {thing: 'a', order: 2}
+    {'thing': 'a', 'order': 3},
+    {'thing': 'a', 'order': 1},
+    {'thing': 'a', 'order': 2}
   ]
-  mergeSort(arr, function(a, b){ return a[order] < b[order] });
+  mergeSort(arr, function(a, b){ return a['order'] < b['order'] });
 */
 
 var mergeSort = function(collection, rule){
