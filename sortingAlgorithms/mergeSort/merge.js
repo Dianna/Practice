e730b02c3933863b03d4887b98b595e5e61b2103
Dianna Faulk <diannafaulk@gmail.com>
2015-09-21 10:49:38 -0700

    Edit merge to handle arrays, split collection in mergeSort

--- sortingAlgorithms/mergeSort/merge.js
+++ sortingAlgorithms/mergeSort/merge.js
@@ -14,13 +14,29 @@ Objects
 */
 
 var mergeSort = function(collection, rule){
-
+  if (collection.length <= 1){
+    return collection;
+  }
+  var middle = Math.floor(collection.length / 2);
+  var left = collection.slice(0, middle);
+  var right = collection.slice(middle, collection.length);
+  return merge(mergeSort(left, rule), mergeSort(right, rule), rule);
 }
 
 var merge = function(item1, item2, rule){
-  if (rule(item1, item2)){
-    return [item1, item2];
-  } else {
-    return [item2, item1];
+  var result = [];
+  while (item1.length && item2.length){
+    if (rule(item1[0], item2[0])){
+      result.push(item1.shift());
+    } else {
+      result.push(item2.shift());
+    }
   }
+  while (item1.length){
+    result.push(item1.shift());
+  }
+  while (item2.length){
+    result.push(item2.shift());
+  }
+  return result;
 }
